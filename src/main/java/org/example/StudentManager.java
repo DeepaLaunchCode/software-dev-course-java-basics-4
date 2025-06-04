4433b1b568bb9bc5c825791b6182e6f7897c0c0e
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-06-04 18:18:27 -0500

    Added sudent manager extercise

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -34,7 +34,10 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.println("Enter your student name: ");
+        String nameofstudent = scanner.nextLine();
+        students.add(nameofstudent);
+        System.out.println("Student " + nameofstudent + " added successfully!");
         mainMenu();
     }
 
@@ -42,14 +45,28 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
-
+        System.out.println("Enter student name to remove: ");
+        String nameofstudent = scanner.nextLine();
+        if (students.contains(nameofstudent)) {
+            students.remove(nameofstudent);
+            System.out.println("Student  named" + nameofstudent + " removed successfully!");
+        } else {
+            System.out.println("Student not found.");
+        }
         mainMenu();
     }
 
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
-
+        if (students.isEmpty()) {
+            System.out.println("No students in the list.");
+        } else {
+            System.out.println("List of students:");
+            for (String nameofstudent : students) {
+                System.out.println("- " + nameofstudent);
+            }
+        }
         mainMenu();
     }
 }
